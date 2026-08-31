133ec048e85da61458a914836ba9c9fcaee262ff
LayF <ninepover@gmail.com>
2026-08-31 17:30:04 +0300

    Remove temp files

--- check-user.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.user.findUnique({ where: { username: 'LayF' } }).then(u => {
-  console.log('passwordHash length:', u?.passwordHash?.length);
-  console.log('passwordHash prefix:', u?.passwordHash?.substring(0, 20));
-  p.$disconnect();
-}).catch(e => { console.error(e.message); p.$disconnect(); });
--- fix-pw.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-(async () => {
-  await p.user.update({ where: { username: 'LayF' }, data: { is2FAEnabled: false, twoFACode: null, twoFACodeExpires: null } });
-  console.log('2FA disabled for LayF');
-  await p.$disconnect();
-})();
